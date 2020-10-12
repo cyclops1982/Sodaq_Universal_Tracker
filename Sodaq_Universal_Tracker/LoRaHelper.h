760b3ebef80f340bbee3d1078aed38b86a4cbbdb
Ruben d'Arco <cyclops-github@prof-x.net>
2020-10-12 21:16:11 +0200

    WIP<M-S-F3>

--- Sodaq_Universal_Tracker/LoRaHelper.h
+++ Sodaq_Universal_Tracker/LoRaHelper.h
@@ -103,7 +103,7 @@ private:
     uint32_t _lastTransmissionAttemptTimestamp;
     uint32_t (*_getNow)();
 
-    bool convertAndCheckHexArray(uint8_t* result, const char* hex, size_t resultSize);
+    bool convertAndCheckHexArray(uint8_t* result, const char* hex, size_t resultSize, bool allowZeros = false);
     bool joinAbp();
     bool joinOtaa();
     void retransmissionUpdateOnSuccess();
